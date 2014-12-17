ed7ec0b4d7c29766fe71d0a21c5d6539e43e47bd
Bragadeesh153 <bragadeesh153@gmail.com>
2014-12-18 02:05:37 +0530

    General method to extract all details without loop

--- personal/bragadeesh/rotentomto2.py
+++ personal/bragadeesh/rotentomto2.py
@@ -1,5 +1,6 @@
 #Program to get synopsis of a movie from rotten tomatoes:
 import urllib2
+import re
 api = '43h3hy2a4r6cuf3h3d4tzkqy'
 print('Enter the name of the movie:')
 name = input()
@@ -7,22 +8,9 @@ req = urllib2.Request('http://api.rottentomatoes.com/api/public/v1.0/movies.json
 response = urllib2.urlopen(req)
 page = response.read()
 print "Start"
-i = 0
-detail = 'none'
-over=0
-while(over!=1):
-    if(page[i]=='"'):
-        i+=1
-        while(page[i]!='"'):
-            detail[0]=Page[i]
-            i+=1
-    if(detail == 'synopsis'):
-        while(page[i]!='"'):
-            i+=1
-        while(page[i]!='"'):
-            synopsis[0]=Page[i]
-            i+=1
-        over=1
+text = '"synopsis":"(.+?)"'
+pattern = re.compile(text)
+synopsis = re.findall(pattern,page)
 print('The synopsis of your movie is described:')
 print synopsis
 print "Stop"
